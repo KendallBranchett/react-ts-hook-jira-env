248be6ad20c19bc1896b3efb7eb5bb92f7ff5ffa
KendallBranchett <1325936293@qq.com>
2024-01-16 21:49:21 +0800

    style: 实现Row组件布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,11 +1,33 @@
-import { useAuth } from "context/auth-context"
-import React from "react"
-import { ProjectListScreen } from "./screens/project-list"
+import { useAuth } from "context/auth-context";
+import React from "react";
+import { ProjectListScreen } from "./screens/project-list";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
-    const {logout} = useAuth()
-    return <div>
-        <button onClick={logout}>登出</button>
+  const { logout } = useAuth();
+  return (
+    <div>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
         <ProjectListScreen></ProjectListScreen>
+      </Main>
     </div>
-}
+  );
+};
+
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  height: 100vh - 6rem;
+`;
