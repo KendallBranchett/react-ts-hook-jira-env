73dde93e458669a445f1d9f32e10b9ac6ac4ca7f
KendallBranchett <1325936293@qq.com>
2024-01-17 14:09:52 +0800

    chore: 配置react-query

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from "react"
 import { AuthProvider } from "./auth-context"
 import React from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 export const AppProviders = ({children}: {children: ReactNode}) => {
-    return <AuthProvider>
+    const queryClient = new QueryClient();
+
+    return <QueryClientProvider client={queryClient}>
+    <AuthProvider>
         {children} 
     </AuthProvider>
+    </QueryClientProvider>
 }
