147763939cecb617ff818f72168cbec62f20ebbf
KendallBranchett <1325936293@qq.com>
2024-01-17 21:34:03 +0800

    chore: 用useRef实现useDocumentTitle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 export const isVoid = (value: unknown) =>
@@ -64,7 +64,7 @@ export const useDocumentTitle = (
   title: string,
   keepOnUnmount: boolean = true
 ) => {
-  const oldTitle = document.title;
+  const oldTitle = useRef(document.title).current;
 
   useEffect(() => {
     document.title = title;
