5c061793cc3e529b81aa8173c9fd07aa24e3c707
KendallBranchett <1325936293@qq.com>
2024-01-17 15:10:24 +0800

    feat: 给页面添加loaing和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,20 +6,23 @@ import { cleanObject, useMount, useDebounce } from "utils";
 import * as qs from "qs";
 import { useHttp } from "utils/http";
 import styled from "@emotion/styled";
+import { Typography } from "antd";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState<null | Error>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
   const debouncedParam = useDebounce(param, 1000);
-  const client = useHttp()
+  const client = useHttp();
 
   useMount(() => {
-    client('users').then(setUsers)
+    client("users").then(setUsers);
     //当用户输入关键词或选择select框时,param变化，param变化时请求工程列表
     // fetch(`${apiUrl}/users`).then(async (res) => {
     //   if (res.ok) {
@@ -30,7 +33,14 @@ export const ProjectListScreen = () => {
   });
 
   useEffect(() => {
-    client('projects', {data: cleanObject(debouncedParam)}).then(setList)
+    setIsLoading(true);
+    client("projects", { data: cleanObject(debouncedParam) })
+      .then(setList)
+      .catch((error) => {
+        setList([]);
+        setError(error);
+      })
+      .finally(() => setIsLoading(false));
     // fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(
     //   async (res) => {
     //     if (res.ok) {
@@ -41,14 +51,17 @@ export const ProjectListScreen = () => {
   }, [debouncedParam]);
 
   return (
-      <Container>
-        <h1>项目列表</h1>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
-      </Container>
+      {error ? (
+        <Typography.Text type={"danger"}>{error.message}</Typography.Text>
+      ) : null}
+      <List loading={isLoading} users={users} dataSource={list} />
+    </Container>
   );
 };
 
 const Container = styled.div`
-  padding: 2.2rem
-`
+  padding: 2.2rem;
+`;
