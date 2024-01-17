ae81a6017498ade46d02c78790280aa0d36c201f
KendallBranchett <1325936293@qq.com>
2024-01-17 14:09:36 +0800

    feat: 实现isVoid方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
-
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
 //在函数中，改变传入的对象本事是不好的
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    //@ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-    //@ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
@@ -19,6 +18,7 @@ export const cleanObject = (object: object) => {
 export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
@@ -55,7 +55,7 @@ export const useArray = <T>(initialArray: T[]) => {
     removeIndex: (index: number) => {
       const copy = [...value];
       copy.splice(index, 1);
-      setValue(copy)
-    }
-  }
-}
+      setValue(copy);
+    },
+  };
+};
