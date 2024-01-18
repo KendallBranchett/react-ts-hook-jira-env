ff968dbf3c2f9a364cd07c8faf5bfe127a26f3e8
KendallBranchett <1325936293@qq.com>
2024-01-18 16:14:53 +0800

    feat: 添加路由projects和project

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -6,41 +6,62 @@ import { ProjectListScreen } from "./screens/project-list";
 import styled from "@emotion/styled";
 import { Row } from "./components/lib";
 import { ReactComponent as SoftwareLogo } from "./assets/software-logo.svg";
-import { Button, Dropdown, Menu, MenuProps } from "antd";
+import { Button, Dropdown, MenuProps } from "antd";
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { ProjectScreen } from "screens/project";
 
 export const AuthenticatedApp = () => {
-  const { logout, user } = useAuth();
-
-  const items: MenuProps['items'] = [
-    {
-      key: 'logout',
-      label: (
-        <Button type={"link"} onClick={logout}>登出</Button>
-        // <a onClick={logout}>登出</a>
-      ),
-    }]
   return (
-    <div>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
-          <h2>项目</h2>
-          <h2>用户</h2>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            menu={{ items }}
-          >
-            <Button type={"link"} onClick={(e) => e.preventDefault()}>Hi, {user?.name}</Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+    <Container>
+      <PageHeader />
       <Main>
-        <ProjectListScreen></ProjectListScreen>
+        <Routes>
+          <Route path={"/projects"} element={<ProjectListScreen />} />
+          <Route path={"/projects/:projectId/*"} element={<ProjectScreen />} />
+        </Routes>
       </Main>
-    </div>
+    </Container>
   );
 };
+
+const PageHeader = () => {
+  const { logout, user } = useAuth();
+
+  const items: MenuProps["items"] = [
+    {
+      key: "logout",
+      label: (
+        <Button type={"link"} onClick={logout}>
+          登出
+        </Button>
+        // <a onClick={logout}>登出</a>
+      ),
+    },
+  ];
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown menu={{ items }}>
+          <Button type={"link"} onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  );
+};
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
 
 const Header = styled(Row)`
   padding: 1.2rem;
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,14 +1,17 @@
-import { ReactNode } from "react"
-import { AuthProvider } from "./auth-context"
-import React from "react"
-import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactNode } from "react";
+import { AuthProvider } from "./auth-context";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router } from "react-router-dom";
 
-export const AppProviders = ({children}: {children: ReactNode}) => {
-    const queryClient = new QueryClient();
+export const AppProviders = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient();
 
-    return <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-        {children} 
-    </AuthProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
     </QueryClientProvider>
-}
+  );
+};
