6c9bf39bc1f4a289b880f1fdb3bb30ab4e27ca78
KendallBranchett <1325936293@qq.com>
2024-01-25 14:31:51 +0800

    feat: 实现看板列表初步功能

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { useDocumentTitle } from "utils";
+import { useKanbans } from "utils/kanban";
+import { useKanbansSearchParams, useProjectInUrl } from "./util";
+import { KanbanColumn } from "./kanban-column";
+import styled from "@emotion/styled";
+import { SearchPanel } from "./search-panel";
 
 export const KanbanScreen = () => {
-  return <h1>Kanban</h1>;
+  useDocumentTitle("看板列表");
+
+  const { data: currentProject } = useProjectInUrl();
+  const { data: kanbans } = useKanbans(useKanbansSearchParams());
+  console.log("currentProject", currentProject);
+  console.log("kanbans", kanbans);
+
+  return (
+    <div>
+      <h1>{currentProject?.name}看板</h1>
+      <SearchPanel />
+      <ColumnsContainer>
+        {kanbans?.map((kanban) => (
+          <KanbanColumn key={kanban.id} kanban={kanban} />
+        ))}
+      </ColumnsContainer>
+    </div>
+  );
 };
+
+const ColumnsContainer = styled.div`
+  display: flex;
+  overflow: hidden;
+  margin-right: 2rem;
+`;
